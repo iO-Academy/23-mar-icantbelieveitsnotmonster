f07fd95c66fecad5dc92469f31e46c91dd9683c8
Phildallen <phil_372@live.com>
2023-05-03 11:52:31 +0100

    (chore) removed semicolons for better consistency

--- src/components/DetailModal/index.js
+++ src/components/DetailModal/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react'
 
 const DetailModal = () => {
 
@@ -101,7 +101,7 @@ const DetailModal = () => {
     )
 }
 
-export default DetailModal;
+export default DetailModal
 
 
 
