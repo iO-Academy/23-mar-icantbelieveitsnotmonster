7a03bde0d1bbb3a687bac697b6a5f6e461a7d16b
Adam Young <adamyoungy678@gmail.com>
2023-05-25 11:37:38 +0100

    fix: added invisible visbility to post a job button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -57,7 +57,7 @@ const Header = ({setSearched, setFullTimeChecked, setPartTimeChecked, setContrac
                             <a className="nav-link" href="#" onClick={handleContractsButton} data-query="type[]=Contract">Contract</a>
                         </li>
                     </ul>
-                    <button className="btn btn-outline-success" type="submit">Post a job</button>
+                    <button className="btn btn-outline-success invisible" type="submit">Post a job</button>
                 </div>
             </div>
         </nav>
